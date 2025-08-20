efeb6be45adad864bbcdf89d2785b6138d71717f
Mohamed Yassin <mhmd.yassin07@gmail.com>
2025-08-20 07:24:55 +0300

    تحديث OwnerResource: إخفاء معلومات الدخول وتوليد البيانات تلقائياً من phone1

--- app/Filament/Resources/OwnerResource/Pages/CreateOwner.php
+++ app/Filament/Resources/OwnerResource/Pages/CreateOwner.php
@@ -18,4 +18,25 @@ class CreateOwner extends CreateRecord
     {
         return 'تم إنشاء المالك بنجاح';
     }
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        // Get domain from APP_URL without http://
+        $appUrl = config('app.url');
+        $domain = str_replace(['http://', 'https://'], '', $appUrl);
+        
+        // Use phone1 as username
+        $data['username'] = $data['phone1'];
+        
+        // Generate email from phone1
+        $data['email'] = $data['phone1'] . '@' . $domain;
+        
+        // Set password as phone1
+        $data['password'] = $data['phone1'];
+        
+        // Set user_type
+        $data['user_type'] = 'owner';
+        
+        return $data;
+    }
 }
