cb152906aabe623004153050338d76fa41d10c61
Mohamed Yassin <mhmd.yassin07@gmail.com>
2026-01-21 17:12:26 +0200

    remove old seeders
    Delete legacy seeder files. Only UsersSeeder is kept for creating default users.
    Reference data will be managed through migrations or imported from external sources.
    
    .

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,22 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            RolesAndPermissionsSeeder::class,
-            UserTypesSeeder::class,
-            LocationSeeder::class,
-            PropertyTypeSeeder::class,
-            PropertyStatusSeeder::class,
-            PropertyFeatureSeeder::class,
-            UnitStatusSeeder::class,
-            UnitFeatureSeeder::class,
-            UnitSeeder::class,
-            ContractStatusSeeder::class,
-            ContractsSeeder::class,
-        ]);
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            UsersSeeder::class,
         ]);
     }
 }
